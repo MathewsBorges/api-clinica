9122413848947e527d08b735649abd192969ddee
unknown <matheusnborgesdev@gmail.com>
2023-03-03 10:11:37 -0300

    api

--- api/server.js
+++ api/server.js
@@ -3,27 +3,56 @@ const mysql = require("mysql2");
 const app = express();
 
 const connection = mysql.createConnection({
-    host: 'db4free.net',
-    user: 'clinicamedica',
-    password: 'clinicamedica',
-    database: 'topicos_clinica'
-})
-
-app.get('/paciente', function (req, res) {
-    connection.query("select * from pacientes", function (error, results, fields){
-        if (error) throw error;
-        res.send(results);
-    })
-})
-
-app.get('/paciente/:id', function (req, res) {
-    connection.query('SELECT * FROM pacientes WHERE id = ?', [req.params.id], function (error, results, fields) {
-        if (error) throw error;
-        res.send(results[0]);
-    });
+  host: "db4free.net",
+  user: "clinicamedica",
+  password: "clinicamedica",
+  database: "topicos_clinica",
 });
 
+app.get("/paciente", function (req, res) {
+  connection.query(
+    "select * from pacientes",
+    function (error, results, fields) {
+      if (error) throw error;
+      res.send(results);
+    }
+  );
+});
+
+app.get("/paciente/:id", function (req, res) {
+  connection.query(
+    "SELECT * FROM pacientes WHERE id = ?",
+    [req.params.id],
+    function (error, results, fields) {
+      if (error) throw error;
+      res.send(results[0]);
+    }
+  );
+});
+
+app.post("/paciente", function (req, res) {
+  const paciente = req.body;
+  connection.query(
+    "INSERT INTO pacientes SET ?",
+    paciente,
+    function (error, results, fields) {
+      if (error) throw error;
+      res.send("Dados inseridos com sucesso!");
+    }
+  );
+});
+
+app.delete("/paciente/:id", function (req, res) {
+  connection.query(
+    "DELETE FROM pacientes WHERE id = ?",
+    [req.params.id],
+    function (error, results, fields) {
+      if (error) throw error;
+      res.send("Registro excluído com sucesso!");
+    }
+  );
+});
 
 app.listen(3000, function () {
-    console.log('Servidor rodando na porta 3000');
+  console.log("Servidor rodando na porta 3000");
 });
